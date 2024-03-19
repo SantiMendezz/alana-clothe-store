74d23ae16b902694a6d64d076f5a5a1ba88653eb
Santiago Mendez <santimendezsam@gmail.com>
2024-03-19 12:34:42 -0300

    Fix 3 Routes

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
       <Header />
       <Navbar />
         <Routes>
-          <Route exact path='/alana-clothe-store/build' element={<Inicio/>} />
+          <Route exact path='/' element={<Inicio/>} />
           <Route path='/contacto' element={<Contacto/>} />
           <Route path='/preguntas-frecuentes' element={<PreguntasFrecuentes/>} />
           <Route path='/productos' element={<Productos/>} />
